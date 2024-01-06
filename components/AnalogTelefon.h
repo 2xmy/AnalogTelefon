6df0f73cb0eb7381932a4bf65099468f86c9c9ef
2xmy <155847673+2xmy@users.noreply.github.com>
2024-01-06 22:45:20 +0100

    Update AnalogTelefon.h

--- components/AnalogTelefon.h
+++ components/AnalogTelefon.h
@@ -1,14 +1,19 @@
-#include "AnalogTelefon.h"
 #include "esphome.h"
 
-void AnalogTelefonComponent::setup() {
-  // Setup code here
-}
-
-void AnalogTelefonComponent::loop() {
-  static unsigned long last_time = 0;
-  if (millis() - last_time > 1000) { // Every 1 second
-    last_time = millis();
-    ESP_LOGI("custom", "One second passed");
+class AnalogTelefonComponent : public Component {
+ public:
+  void setup() override {
+    // Setup code here
   }
-}
+
+  void loop() override {
+    static unsigned long last_time = 0;
+    if (millis() - last_time > 1000) { // Every 1 second
+      last_time = millis();
+      ESP_LOGI("custom", "One second passed");
+    }
+  }
+};
+
+// Registration macro
+ESPHOME_REGISTER_COMPONENT(AnalogTelefonComponent, AnalogTelefonComponent)
